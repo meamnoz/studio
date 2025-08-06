e4b9afd9169774c984fc6b1c93922d49a9bfc476
meamnoz <meamnoz@gmail.com>
2025-08-06 14:54:15 +0330

    Create project.js

---
+++ schemas/project.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'summary',
+      title: 'Summary',
+      type: 'text',
+    },
+  ],
+}
